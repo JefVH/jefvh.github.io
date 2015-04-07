bd66b23aefb7d3de40069b035005e6468b6d32c5
JefVH <BEJHW@LABODEV12.deceuninck.com>
2015-04-07 10:28:56 +0200

    File Hierarchy DECEUNINCK

---
+++ deceuninckinspiratie/default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Status = "301 Moved Permanently";
+        Response.AddHeader("Location", "http://www.deceuninck.com/");
+    }
+}
