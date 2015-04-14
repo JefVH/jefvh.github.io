f7154b762326460ff5b7c7010821c3a0e57bd436
JefVH <BEJHW@LABODEV12.deceuninck.com>
2015-04-14 12:52:20 +0200

    sharePreview

--- sharePreview/sharePreview.js
+++ sharePreview/sharePreview.js
@@ -1,17 +1,17 @@
-﻿$(document).ready(function () {
-    $('#submit').on('click', function () {
-        var url = 'https://graph.facebook.com/' + $('#url').val() + '?callback=?&scrape=true&method=post';
+﻿var fburl = "http://graph.facebook.com/http://jefvh.github.io/facebookShare/index.html?callback=?&method=post&scrape=true"
 
-        $.getJSON(url, function (data) {
-            var url  = data['url'];
-            var type = data['type'];
-            var title = data['title'];
-            var description = data['description'];
+$.getJSON(fburl, function (data) {
 
-            $('body').append('<p>URL:' + url + '</p>');
-            $('body').append('<p>TYPE:' + type + '</p>');
-            $('body').append('<p>TITLE:' + title + '</p>');
-            $('body').append('<p>DESCRIPTION:' + description + '</p>');
-        });
-    });
+    var title = data['title'];
+    var url = data['url'];
+    var image = data['image'];
+    var imageUrl = image[0].url;
+    var imageWidth = image[0].width;
+    var imageHeight = image[0].height;
+    $("#profile").append('<h3>' + title + '</h3>');
+    $("#profile").append('<h3>' + url + '</h3>');
+    $("#profile").append('<h3>' + imageUrl + '</h3>');
+    $("#profile").append('<h3>' + imageWidth + '</h3>');
+    $("#profile").append('<h3>' + imageHeight + '</h3>');
+
 });
