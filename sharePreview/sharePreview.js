b928ce07fb9c001d10fb9c0af3e674237611d134
JefVH <BEJHW@LABODEV12.deceuninck.com>
2015-04-14 10:35:03 +0200

    facebook share

--- sharePreview/sharePreview.js
+++ sharePreview/sharePreview.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(document).ready(function () {
+    $('#submit').on('click', function () {
+        var url = 'https://graph.facebook.com/?id=' + $('#url').val() + '&scrape=true&method=post';
+        $.getJSON(url, function (data) {
+            var items = [];
+            $.each(data, function (key, val) {
+                items.push('<li id=' + key + '>' + val + '</li>');
+            });
+
+            $('<ul/>', {
+                'class': 'opengraph',
+                html: items.join('')
+            }).appendTo('body');
+        });
+    });
+});
